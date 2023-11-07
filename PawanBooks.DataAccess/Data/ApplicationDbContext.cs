8e1caa73cc9efc7fbc5c344a4aef468da3177373
PawanSekhon <133436526+PawanSekhon@users.noreply.github.com>
2023-11-07 14:25:44 -0500

    Installed Identity.EntityFrameworkCore and edited the namespace in .DataAccess project to reflect namespace.

--- PawanBooks.DataAccess/Data/ApplicationDbContext.cs
+++ PawanBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace PawanSekhonBookStore.Data
+namespace PawanSekhonBookStore.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
