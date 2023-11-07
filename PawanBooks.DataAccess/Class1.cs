adc23f592d96ed4f638c0b2f055889b935a0d7b1
PawanSekhon <133436526+PawanSekhon@users.noreply.github.com>
2023-11-07 14:27:43 -0500

    Delete default Class1.cs files in all three projects

--- PawanBooks.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PawanBooks.DataAccess
-{
-    public class Class1
-    {
-    }
-}
--- PawanBooks.Utility/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PawanBooks.Utility
-{
-    public class Class1
-    {
-    }
-}
