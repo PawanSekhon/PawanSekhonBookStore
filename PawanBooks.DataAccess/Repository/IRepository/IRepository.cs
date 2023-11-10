4ae726a97ab8366f7ee86a6e9494f5fbaf83895c
PawanSekhon <133436526+PawanSekhon@users.noreply.github.com>
2023-11-10 00:14:36 -0500

    modify IRepository interface

--- PawanBooks.DataAccess/Repository/IRepository/IRepository.cs
+++ PawanBooks.DataAccess/Repository/IRepository/IRepository.cs
@@ -8,11 +8,16 @@ namespace PawanBooks.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        T Get(int id);
+        T Get(int id); //retrieve a category from the database by id 
+        //List of categories based on requirements
         IEnumerable<T> GetAll(
             Expression<Func<T, bool>> filter = null,
             Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-            string includeProperties = null
+            string includeProperties = null //useful for foreign key references
             );
+        void Add(T entity); //to add an entity
+        void Remove(int id); //to remove an object or category
+        void Remove(T entity); //another way to remove an object
+        void RemoveRange(IEnumerable<T> entity);
     }
 }
