fef2eda10eb695c5e2b803195b5d795f57fa7d5d
PawanSekhon <133436526+PawanSekhon@users.noreply.github.com>
2023-11-20 23:34:06 -0500

    added CoverType.cs and ICoverType.cs in Repository and IRepository respectively

---
+++ PawanBooks.DataAccess/Repository/IRepository/ICoverType.cs
@@ -0,0 +1,13 @@
+﻿using PawanBooks.DataAccess.Repository.ICoverTypeRepository;
+using PawanBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PawanBooks.DataAccess.Repository.IRepository
+{
+    public interface ICoverType
+    {
+        void Update(CoverType coverType);
+    }
+}
